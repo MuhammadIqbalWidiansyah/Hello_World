635047dd2a87b18ea6d3f7e99acfed8d38f1ddb2
Muhammad Iqbal Widiansyah <92410990+MuhammadIqbalWidiansyah@users.noreply.github.com>
2021-11-24 09:08:27 +0700

    Add files via upload

---
+++ Hello World.cpp
@@ -0,0 +1,21 @@
+//karena menggunakan cout
+#include <iostream>
+
+//karena menggunakan getch()
+#include <conio.h>
+
+//untuk mempersingkat penulisan
+using namespace std;
+
+//fungsi utama
+int main()
+{
+    //untuk menampilkan output
+    cout << "Hello World" << endl;
+
+    //agar program tidak langsung berhenti
+    getch();
+
+    //agar program berakhir dengan nilai 0
+    return 0;
+}
